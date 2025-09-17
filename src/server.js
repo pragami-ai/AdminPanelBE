615d2c1183ee6f98b1be6b69abd108cf6ddb56f9
parthpragami <parth@pragami.com>
2025-09-17 17:41:01 +0530

    removed unused code

--- src/server.js
+++ src/server.js
@@ -96,6 +96,7 @@ import { adminGetUserSelections } from './booking/getUserSelections.js';
 import { adminEditBooking } from './booking/editBooking.js';
 import { getCostReport } from './cost/costTracking.js';
 import { adminGetTranscriptSummary } from './booking/getTranscriptSummary.js';
+import {getForm} from "./forms/crud.js";
 
 
 
@@ -200,149 +201,18 @@ export const app = async (event, context, requestId) => {
         };
     }
     switch (event.rawPath) {
-        case API_PATHS.LOGIN: {
-            return await login(body);
-        }
-        case API_PATHS.GOOGLE_SIGNUP: {
-            return await googleSignup(body);
-        }
-        case API_PATHS.GOOGLE_LOGIN: {
-            return await googleLogin(body);
-        }
-        case API_PATHS.SIGNUP: {
-            return await signup(body);
-        }
-        case API_PATHS.VERIFY_OTP: {
-            return await verifyOtp(body);
-        }
-        case API_PATHS.HELLO: {
-            // Test route, to be removed later
-            return await hello();
-        }
-        case API_PATHS.CREATE_ROLE: {
-            return await createUserRole(body);
-        }
-        case API_PATHS.USER_INFORMATION: {
-            return await userInformation(body); // update profile
-        }
-        case API_PATHS.ME: {
-            return await me(body); // fetch profile
-        }
-        case API_PATHS.CREATE_IDEA: {
-            // Idea Capture, when user enters details and pitch deck files
-            return await createIdeaAPI(body);
-        }
-        case API_PATHS.GET_LATEST_IDEA: {
-            // TODO: check usage
-            return await getLatestIdea(body);
-        }
-        case API_PATHS.STORE_IDEA: {
-            // When user accepts AI generated idea response
-            return await storeIdea(body);
-        }
-        case API_PATHS.REGENERATE_IDEA: {
-            // When user asks to regenerate AI response
-            return await regenerateIdea(body);
-        }
-        case API_PATHS.GET_STORE_IDEA: {
-            // In case user goes back to idea capture phase
-            return await getStoreIdea(body);
-        }
-        case API_PATHS.STORE_BURNING_PROBLEM: {
-            // Stores users entered burning problems
-            return await storeBurningProblem(body);
-        }
-        case API_PATHS.IDEA_LENS_SELECTOR: {
-            // Last step of idea capture phase, calls Lens Selector AI
-            return await ideaLensSelector(body);
-        }
-        case API_PATHS.GET_IDEAS: {
-            return await getIdeasAPI(body);
-        }
-        case API_PATHS.GET_IDEA_BY_ID: {
-            return await getIdeaById(body);
-        }
-        case API_PATHS.IDEA_SURVEY_GENERATOR: {
-            return await ideaSurveyGenerator(body);
-        }
-        case API_PATHS.CREATE_BOOKING: {
-            return await createBookingAPI(body);
-        }
+
         case API_PATHS.UPDATE_BOOKING: {
             return await updateBookingAPI(body);
-        }
-        // for GET request with path params, best practice is to use routeKey instead of rawPath, so converting GET routes to POST.
-        case API_PATHS.GET_BOOKING: {
-            return await getBookingAPI(body);
-        }
-        case API_PATHS.GET_BOOKINGS: {
-            return await getBookingsAPI(body);
         }
         case API_PATHS.CREATE_MEETING: {
             return await createMeeting(body);
         }
-        case API_PATHS.GET_AVAILABLE_SLOTS: {
-            return await getAvailableSlots(body);
-        }
-        case API_PATHS.FORGOT_PASSWORD: {
-            return await forgotPassword(body);
-        }
-        case API_PATHS.RESEND_OTP: {
-            return await resendOTP(body);
-        }
-        case API_PATHS.RESET_PASSWORD: {
-            body.token = extractTokenFromHeaders(event.headers);
-            return await resetPassword(body);
-        }
-        case API_PATHS.ONBOARD_USER: {
-            return await onBoardUser(body);
-        }
+
         case API_PATHS.GET_FORM: {
             return await getForm(body);
         }
-        case API_PATHS.RETRY_FORM: {
-            return await createFormS3LinkAPI(body);
-        }
-        case API_PATHS.CREATE_FORM: {
-            return await createFormAPI(body);
-        }
-        case API_PATHS.GET_FORM_RESPONSE: {
-            return await getFormResponseAPI(body);
-        }
-        case API_PATHS.GET_ALL_FORM_RESPONSE: {
-            return await getFormResponsesAPI(body);
-        }
-        case API_PATHS.RETRY_FORM_RESPONSE: {
-            return await createFormResponseS3LinkAPI(body);
-        }
-        case API_PATHS.CREATE_FORM_RESPONSE: {
-            return await createResponse(body);
-        }
-        case API_PATHS.SEARCH_USERS: {
-            return await searchUsersAPI(body);
-        }
-        case API_PATHS.GET_PUBLIC_FORM: {
-            return await getPublicForm(body);
-        }
-        case API_PATHS.GET_FORMS: {
-            return await getForms(body);
-        }
-        case API_PATHS.GET_USER_DETAILS: {
-            return await getUserDetails(body);
-        }
-        case API_PATHS.SME_MATCHMAKING: {
-            return await smeMatchmakingAPI(body);
-        }
-        case API_PATHS.GET_USER_CONSENT: {
-            return await getUserConsent(body);
-        }
-        case API_PATHS.UPDATE_USER_CONSENT: {
-            return await updateUserConsent(body);
-        }
 
-        case API_PATHS.GOOGLE_AUTH: {
-            return await googleLoginOrSignup(body);
-        }
         /**
          * ADMIN ROUTES
          */
@@ -363,11 +233,6 @@ export const app = async (event, context, requestId) => {
         case ADMIN_API_PATHS.MY_IDEA: {
             return await myIdea(body);
         }
-        case ADMIN_API_PATHS.AVAILABLE_SLOTS: {
-            return await getAvailableSlots(body);
-        }
-
-        
 
                 // ===== ANALYTICS LAMBDA FUNCTIONS =====
         case ADMIN_API_PATHS.HEALTH: {
@@ -423,9 +288,6 @@ export const app = async (event, context, requestId) => {
         case ADMIN_API_PATHS.CREATE_USER: {
             return await adminCreateUser(body);
         }
-        case ADMIN_API_PATHS.DELETE_USER: {
-            return await adminDeleteUser(body);
-        }
         case ADMIN_API_PATHS.GET_ALL_USERS: {
             return await getAllUsers(body);
         }
@@ -476,9 +338,6 @@ export const app = async (event, context, requestId) => {
         case ADMIN_API_PATHS.TRANSCRIPT: {
             return await adminGetTranscriptSummary(body);
         }
-
-        
-        
 
 
         default: {
